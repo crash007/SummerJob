06b3fc8aedd1d6650dd5c7531e9fa3f35898957d
Petter Wallin <wallin.petter@gmail.com>
2015-10-29 13:29:48 +0100

    added unimplemented method

--- src/se/sogeti/jobapplications/beans/business/BusinessSectorJobApplication.java
+++ src/se/sogeti/jobapplications/beans/business/BusinessSectorJobApplication.java
@@ -1,15 +1,14 @@
 package se.sogeti.jobapplications.beans.business;
 
-import java.sql.Date;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 import se.sogeti.jobapplications.beans.DriversLicenseType;
 import se.sogeti.jobapplications.beans.JobApplication;
-import se.sogeti.jobapplications.beans.municipality.MunicipalityJob;
 import se.unlogic.standardutils.dao.annotations.DAOManaged;
-import se.unlogic.standardutils.dao.annotations.Key;
 import se.unlogic.standardutils.dao.annotations.ManyToOne;
 import se.unlogic.standardutils.dao.annotations.Table;
-import se.unlogic.standardutils.xml.XMLElement;
+import se.unlogic.standardutils.xml.XMLGenerator;
 
 @Table(name="summer_job_business_sector_job_application")
 public class BusinessSectorJobApplication extends JobApplication{
@@ -22,5 +21,11 @@ public class BusinessSectorJobApplication extends JobApplication{
 	@DAOManaged(columnName="driversLicenseTypeId")
 	@ManyToOne(remoteKeyField="id")
 	private DriversLicenseType driversLicenseType;
+
+	@Override
+	public Element toXML(Document doc) {
+
+		return XMLGenerator.toXML(this, doc);
+	}
 
 }
