dbf9452441c8f96afd474b94907dc8994f6acd26
Petter Wallin <wallin.petter@gmail.com>
2016-01-07 13:22:31 +0100

    Modul för välkommstsidan

---
+++ src/se/sogeti/summerjob/welcome/SummerJobWelcomeModule.java
@@ -0,0 +1,33 @@
+package se.sogeti.summerjob.welcome;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import se.unlogic.hierarchy.core.beans.SimpleForegroundModuleResponse;
+import se.unlogic.hierarchy.core.beans.User;
+import se.unlogic.hierarchy.core.interfaces.ForegroundModuleResponse;
+import se.unlogic.hierarchy.foregroundmodules.AnnotatedForegroundModule;
+import se.unlogic.standardutils.xml.XMLUtils;
+import se.unlogic.webutils.http.RequestUtils;
+import se.unlogic.webutils.http.URIParser;
+
+public class SummerJobWelcomeModule extends AnnotatedForegroundModule {
+
+	@Override
+	public ForegroundModuleResponse defaultMethod(HttpServletRequest req, HttpServletResponse res, User user,
+			URIParser uriParser) throws Throwable {
+		
+
+		Document doc = XMLUtils.createDomDocument();
+		Element element = doc.createElement("Document");
+		element.appendChild(RequestUtils.getRequestInfoAsXML(doc, req, uriParser));
+		element.appendChild(this.sectionInterface.getSectionDescriptor().toXML(doc));
+		element.appendChild(this.moduleDescriptor.toXML(doc));
+		doc.appendChild(element);
+		return new SimpleForegroundModuleResponse(doc);
+	}
+
+}
---
+++ src/se/sogeti/summerjob/welcome/SummerJobWelcomeModuleTemplates.xsl
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+	<xsl:output method="html" version="4.0" encoding="ISO-8859-1"/>
+	
+	<xsl:template match="Document">
+		<h1>Sundsvalls kommuns sommarjobbsportal</h1>
+		<p class="lead">Här kan du som är skriven i Sundsvalls kommun och går på en gymnasieskola söka sommarjobb.</p>
+		
+		<h2>Sök sommarjobb inom Sundsvalls Kommun</h2>
+		<p class="lead">Vi erbjuder sommarjobb inom kommunens olika förvaltningar.</p>
+		<a href="#">Skicka in din ansökan</a>
+		
+		<h2>Sommarjobb inom näringslivet</h2>
+		<p class="lead">Sundsvalls kommun förmedlar sommarjobb inom näringslivet.</p>
+		<a href="" class="business-jobs-link">Listan över lediga sommarjobb</a>
+		
+	</xsl:template>
+</xsl:stylesheet>
