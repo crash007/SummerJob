78aebb2f848bc60fc2fdf63bb994e2bbaed0f6a9
Petter Johansson <cadaver.pete@gmail.com>
2016-02-03 10:31:45 +0100

    Modulinställningar för att stänga av länkar och visa meddelanden på välkomstsidan.

--- src/se/sogeti/summerjob/welcome/SummerJobWelcomeModule.java
+++ src/se/sogeti/summerjob/welcome/SummerJobWelcomeModule.java
@@ -8,7 +8,9 @@ import org.w3c.dom.Element;
 
 import se.sogeti.summerjob.addsummerjobapplication.AddMunicipalityJobApplicationHandler;
 import se.sogeti.summerjob.addsummerjobapplication.BusinessSectorJobApplicationHandler;
+import se.unlogic.hierarchy.core.annotations.CheckboxSettingDescriptor;
 import se.unlogic.hierarchy.core.annotations.InstanceManagerDependency;
+import se.unlogic.hierarchy.core.annotations.ModuleSetting;
 import se.unlogic.hierarchy.core.beans.SimpleForegroundModuleResponse;
 import se.unlogic.hierarchy.core.beans.User;
 import se.unlogic.hierarchy.core.interfaces.ForegroundModuleResponse;
@@ -18,6 +20,22 @@ import se.unlogic.webutils.http.RequestUtils;
 import se.unlogic.webutils.http.URIParser;
 
 public class SummerJobWelcomeModule extends AnnotatedForegroundModule {
+	
+	@ModuleSetting
+	@CheckboxSettingDescriptor(description="Bocka i denna när tiden för att lägga upp jobb passerat", name = "Kommunala jobb har passerat")
+	private boolean municipalityJobExceeded = false;
+	
+	@ModuleSetting
+	@CheckboxSettingDescriptor(description="Bocka i denna när tiden för att lägga upp jobb passerat", name = "Privata jobb har passerat")
+	private boolean businessJobExceeded = false;
+	
+	@ModuleSetting
+	@CheckboxSettingDescriptor(description="Bocka i denna när tiden för att lägga upp ansökningar passerat", name = "Kommunala ansökningar har passerat")
+	private boolean municipalityAppExceeded = false;
+	
+	@ModuleSetting
+	@CheckboxSettingDescriptor(description="Bocka i denna när tiden för att lägga upp ansökningar passerat", name = "Privata ansökningar har passerat")
+	private boolean businessAppExceeded = false;
 	
 	@InstanceManagerDependency(required=true)
 	private AddMunicipalityJobApplicationHandler addMunicipalityJobApplicationHandler;
@@ -36,6 +54,11 @@ public class SummerJobWelcomeModule extends AnnotatedForegroundModule {
 		element.appendChild(this.sectionInterface.getSectionDescriptor().toXML(doc));
 		element.appendChild(this.moduleDescriptor.toXML(doc));
 		doc.appendChild(element);
+		
+		XMLUtils.appendNewElement(doc, element, "municipalityJobExceeded", municipalityJobExceeded);
+		XMLUtils.appendNewElement(doc, element, "businessJobExceeded", businessJobExceeded);
+		XMLUtils.appendNewElement(doc, element, "municipalityAppExceeded", municipalityAppExceeded);
+		XMLUtils.appendNewElement(doc, element, "businessAppExceeded", businessAppExceeded);
 		
 		XMLUtils.appendNewElement(doc, element,"AddMunicipalityApplicationUrl",req.getContextPath()+addMunicipalityJobApplicationHandler.getUrl());
 		XMLUtils.appendNewElement(doc, element,"AddBusinessApplicationUrl",req.getContextPath()+businessSectorJobApplicationHandler.getUrl());
